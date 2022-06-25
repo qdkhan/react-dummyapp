bc22e5d51ae411e48e64cd3f75a511cb02884e72
qdkhan <qdkhan05@gmail.com>
2022-06-25 22:59:03 +0530

    some-changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Qutbuddin Khan</h3>
+        <h4>(+91) 9616279623</h4>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
